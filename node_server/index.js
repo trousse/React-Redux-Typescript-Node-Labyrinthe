b39eac4982ffb6f19e9623e56aef33f2efa61014
Rousse Thomas <rousse_t@ad-its.credit-agricole.fr>
2018-04-17 14:59:36 +0200

    tom

--- node_server/index.js
+++ node_server/index.js
@@ -1,6 +1,10 @@
 var server = require('http').createServer()
 var io = require('socket.io')(server)
-import NewClient from "./client.js"
+import NewClient from "./socket.js"
 
+io.use((socket,next)=>{
+    console.log(JSON.stringify(socket))
+})
 io.on('connection',NewClient(client))
+
 server.listen(8080);
---
+++ node_server/socket.js
@@ -0,0 +1,5 @@
+const NewClient = function(client){
+    client.on()
+}
+
+export default NewClient
